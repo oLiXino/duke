00cc12a35fe109f2c971726712a7e16caaa69a18
Li Xin <llxin900911@gmail.com>
2020-01-22 23:04:49 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,7 @@ public class Duke {
                     System.out.println(i + "." + t);
                     i++;
                 }
-            }  else if (command.contains("done")) {
+            } else if (command.indexOf("done") == 0) {
                 int index = Integer.parseInt(command.split(" ")[1]) - 1;
                 if (index < list.size()) {
                     System.out.println("Nice! I've marked this task as done:");
@@ -29,10 +29,31 @@ public class Duke {
                 } else {
                     System.out.println("This task does not exist in your list");
                 }
-
+            } else if (command.indexOf("todo") == 0) {
+                int emptyIndex = command.indexOf(" ");
+                Todo todo = new Todo(command.substring(emptyIndex + 1));
+                list.add(todo);
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(todo);
+                System.out.println("Now you have " + list.size() + " tasks in the list." );
+            } else if (command.indexOf("deadline") == 0) {
+                int emptyIndex = command.indexOf(" ");
+                int slashIndex = command.indexOf("/");
+                Deadline deadline = new Deadline(command.substring(emptyIndex + 1, slashIndex - 1), command.substring(slashIndex + 4));
+                list.add(deadline);
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(deadline);
+                System.out.println("Now you have " + list.size() + " tasks in the list.");
+            } else if (command.indexOf("event") == 0) {
+                int emptyIndex = command.indexOf(" ");
+                int slashIndex = command.indexOf("/");
+                Event event = new Event(command.substring(emptyIndex + 1, slashIndex - 1), command.substring(slashIndex + 4));
+                list.add(event);
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(event);
+                System.out.println("Now you have " + list.size() + " tasks in the list." );
             } else {
-                list.add(new Task(command));
-                System.out.println("added: " + command);
+                System.out.println("Invalid command");
             }
             System.out.println("-----------------------------------");
             System.out.println();
