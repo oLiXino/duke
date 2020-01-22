b4b74630d2fe188fdea7e14e31cf0b8d0cd9736e
Li Xin <llxin900911@gmail.com>
2020-01-22 22:23:38 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         System.out.println("-----------------------------------");
         System.out.println("Hello! I'm Cat");
         System.out.println("What can I do for you?");
@@ -13,13 +13,25 @@ public class Duke {
         while (!command.equals("bye")) {
             System.out.println("-----------------------------------");
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 int i = 1;
-                for (String s : list) {
-                    System.out.println(i + ". " + s);
+                for (Task t : list) {
+                    System.out.println(i + "." + t);
                     i++;
                 }
+            }  else if (command.contains("done")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                if (index < list.size()) {
+                    System.out.println("Nice! I've marked this task as done:");
+                    Task task = list.get(index);
+                    task.markAsDone();
+                    System.out.println(task);
+                } else {
+                    System.out.println("This task does not exist in your list");
+                }
+
             } else {
-                list.add(command);
+                list.add(new Task(command));
                 System.out.println("added: " + command);
             }
             System.out.println("-----------------------------------");
